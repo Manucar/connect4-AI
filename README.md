0e9570440d0513106493ca9d8db6288e955e5888
Manucar <carzaniga.manuel@gmail.com>
2020-01-09 20:02:42 +0100

    Added check winner function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Connect4-AI
+A simple game in C that implements minimax algorithm with alfa-beta pruning optimization.
--- connect4.c
+++ connect4.c
@@ -7,25 +7,29 @@ void initBoard();
 void drawBoard();
 
 void addMove(char c);
+void addAIMove();
+char checkWin();
 
 int main()
 {
-	char column=99; //random init to enter the while
+	char column;
+	char end = 0;
 	initBoard();
 	printf("Connect 4 - AI\n");
 	//end game choice=0
-	while(column!=0){
-		drawBoard();
+	while(!end){
+		
 		printf("Insert your move: [columns from 1 to 7]\n");
 		scanf("%d", &column);
 		addMove(column);
-		//printf("%c\n", column);
-
-
+		addAIMove();
+		end = checkWin();
+		drawBoard();
 
 	}
 
-    return 0;
+	printf("The winner is Player %d\n", end);
+	return 0;
 }
 
 void initBoard(){
@@ -45,7 +49,6 @@ void drawBoard(){
         printf("|\n");
 	}
 	printf("\n");
-
 }
 
 void addMove(char c){
@@ -55,6 +58,69 @@ void addMove(char c){
 			break;
 		}
 	}
+}
 
+char checkWin(){
+	char winner=0; // 0 indicates no winner yet
+	//check vertically
+	for(int r=num_rows-1;r>=num_rows-3;r--){
+		for(int c=0;c<num_cols;c++){
+			if(board[r][c]=='X' && board[r-1][c]=='X' && board[r-2][c]=='X' && board[r-3][c]=='X'){
+				printf("wins vertically!\n");
+				winner=1;
+			}
+			else if(board[r][c]=='O' && board[r-1][c]=='O' && board[r-2][c]=='O' && board[r-3][c]=='O'){
+				printf("wins vertically!\n");
+				winner=2;
+			}
+		}
+	}
+
+	//check horizontally
+	for(int r=num_rows-1;r>=0;r--){
+		for(int c=0;c<4;c++){
+			if(board[r][c]=='X' && board[r][c+1]=='X' && board[r][c+2]=='X' && board[r][c+3]=='X'){
+				printf("wins horizontally!\n");
+				winner=1;
+			}
+			else if(board[r][c]=='O' && board[r][c+1]=='O' && board[r][c+2]=='O' && board[r][c+3]=='O'){
+				printf("wins horizontally!\n");
+				winner=2;
+			}
+		}
+	}
+
+	//check diagonals 1
+	for(int r=num_rows-1;r>=num_rows-3;r--){
+		for(int c=0;c<4;c++){
+			if(board[r][c]=='X' && board[r-1][c+1]=='X' && board[r-2][c+2]=='X' && board[r-3][c+3]=='X'){
+				printf("wins right diagonal!\n");
+				winner=1;
+			}
+			else if(board[r][c]=='O' && board[r-1][c+1]=='O' && board[r-2][c+2]=='O' && board[r-3][c+3]=='O'){
+				printf("wins right diagonal!\n");
+				winner=2;
+			}
+		}
+	}
+
+	//check diagonals 2
+	for(int r=num_rows-1;r>=num_rows-3;r--){
+		for(int c=3;c<7;c++){
+			if(board[r][c]=='X' && board[r-1][c-1]=='X' && board[r-2][c-2]=='X' && board[r-3][c-3]=='X'){
+				printf("wins left diagonal!\n");
+				winner=1;
+			}
+			else if(board[r][c]=='O' && board[r-1][c-1]=='O' && board[r-2][c-2]=='O' && board[r-3][c-3]=='O'){
+				printf("wins left diagonal!\n");
+				winner=2;
+			}
+		}
+	}
+
+	return winner;
+}
+
+void addAIMove(){
 
 }
